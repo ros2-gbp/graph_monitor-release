1557882fa106da4074992f0fd6182c566348aa75
Emerson Knapp <emerson.b.knapp@gmail.com>
2025-10-15 00:02:49 -0700

    Rebase from 'upstream'

--- rosgraph_monitor/include/rosgraph_monitor/node.hpp
+++ rosgraph_monitor/include/rosgraph_monitor/node.hpp
@@ -16,19 +16,22 @@
 #define ROSGRAPH_MONITOR__NODE_HPP_
 
 #include <memory>
+#include <string>
 #include <vector>
 
 #include "diagnostic_msgs/msg/diagnostic_array.hpp"
 #include "diagnostic_msgs/msg/diagnostic_status.hpp"
 #include "rclcpp/node.hpp"
+#include "rosgraph_monitor_msgs/msg/graph.hpp"
 #include "rosgraph_monitor_msgs/msg/topic_statistics.hpp"
 
 #include "rosgraph_monitor/rosgraph_monitor_generated_parameters.hpp"
-#include "rosgraph_monitor/graph_analyzer.hpp"
 #include "rosgraph_monitor/monitor.hpp"
 
 namespace rosgraph_monitor
 {
+
+constexpr int SERVICE_TIMEOUT_S = 5;
 
 class Node : public rclcpp::Node
 {
@@ -40,25 +43,24 @@ public:
   explicit Node(const rclcpp::NodeOptions & options);
 
 protected:
-  rcl_interfaces::msg::SetParametersResult on_parameter_event(
-    const std::vector<rclcpp::Parameter> & parameters);
-  void on_new_params();
+  void update_params(const rosgraph_monitor::Params & params);
   void on_topic_statistics(const rosgraph_monitor_msgs::msg::TopicStatistics::SharedPtr msg);
   void publish_diagnostics();
+  void publish_rosgraph(rosgraph_monitor_msgs::msg::Graph rosgraph_msg);
+  QueryParamsReturnType query_params(
+    const std::string & node_name,
+    std::function<void(const rcl_interfaces::msg::ListParametersResult &)> callback);
 
-  std::shared_ptr<rclcpp::node_interfaces::OnSetParametersCallbackHandle> param_cb_handle_;
-  std::shared_ptr<rosgraph_monitor::ParamListener> param_listener_;
+  rosgraph_monitor::ParamListener param_listener_;
   rosgraph_monitor::Params params_;
 
   RosGraphMonitor graph_monitor_;
-  GraphAnalyzer graph_analyzer_;
 
-  rclcpp::TimerBase::SharedPtr timer_publish_report_;
   rclcpp::Subscription<rosgraph_monitor_msgs::msg::TopicStatistics>::SharedPtr
     sub_topic_statistics_;
   rclcpp::Publisher<diagnostic_msgs::msg::DiagnosticArray>::SharedPtr pub_diagnostics_;
-  rclcpp::Publisher<diagnostic_msgs::msg::DiagnosticArray>::SharedPtr pub_diagnostic_agg_;
-  rclcpp::Publisher<diagnostic_msgs::msg::DiagnosticStatus>::SharedPtr pub_diagnostic_toplevel_;
+  rclcpp::Publisher<rosgraph_monitor_msgs::msg::Graph>::SharedPtr pub_rosgraph_;
+  rclcpp::TimerBase::SharedPtr timer_publish_report_;
 };
 
 }  // namespace rosgraph_monitor
